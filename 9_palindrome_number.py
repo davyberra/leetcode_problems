a35071c7255cd3c4c792ebad96abcc0515b7fa9e
unknown <davyberra@gmail.com>
2021-01-28 20:39:31 -0800

    Finished problem 9

--- 9_palindrome_number.py
+++ 9_palindrome_number.py
@@ -1,13 +1,15 @@
 class Solution:
     def is_palindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
         x_og = x
         reversed = 0
         for _ in range(len(str(x))):
             reversed = (reversed * 10) + x % 10
             x = round(x / 10)
 
-            print(reversed)
+            # print(reversed)
         return reversed == x_og
 
 s = Solution()
-print(s.is_palindrome(-34543))
+print(s.is_palindrome(0))
