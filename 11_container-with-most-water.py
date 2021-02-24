38ad3cf6e19781d0c07fca0d224f755130b045c1
unknown <davyberra@gmail.com>
2021-02-23 17:21:22 -0800

    started problem 11

---
+++ 11_container-with-most-water.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxArea(self, height: list) -> int:
+        output = 0
+        for index1, num1 in enumerate(height):
+            for index2, num2 in enumerate(height[index1 + 1:]):
+                if num2 >= num1:
+                    area = num1 * (index2 + 1)
+                    if area > output:
+                        output = area
+
+        return output
+
+
+s = Solution()
+print(s.maxArea([1,8,6,2,5,4,8,3,7]))
+print(s.maxArea([1,1]))
+print(s.maxArea([4,3,2,1,4]))
+print(s.maxArea([1,2,1]))
